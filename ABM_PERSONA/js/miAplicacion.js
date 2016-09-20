078bf2c78c9624956758bca53d680812ff6943a2
Jonathan Aguirre <yonathanaguirre@live.com>
2016-09-20 09:32:19 -0300

    Modificar funcionando
    modificar funcionando

--- ABM_PERSONA/js/miAplicacion.js
+++ ABM_PERSONA/js/miAplicacion.js
@@ -120,7 +120,7 @@ miApp.config(function($stateProvider,$urlRouterProvider){
 				}
 			}).state('modificacion',
 	{url: '/modificacion/{id}?:nombre:apellido:dni:foto',
-	templateUrl: 'alta.html',
+	templateUrl: './AbmPersona/personaModificar.html',
 	controller: 'controlModificacion'})
 
 
@@ -265,35 +265,11 @@ $http.post("PHP/nexo.php",{datos:{accion :"borrar",persona:persona}},{headers: {
 
  	}
 
-$scope.Modificar=function(persona){
- 		$http.post('PHP/nexo.php', { datos: {accion :"modificar",persona:personaA}})
-		  .then(function(respuesta) {     
-		  
-				 //aca se ejetuca si retorno sin errores      	
-			 console.log(respuesta.data);
-		 persona.nombre = personaA.nombre;
-			 persona.apellido = personaA.apellido;
-			 persona.dni = personaA.dni;
-			$state.go("persona.Alta");
-		
 
-		},function errorCallback(response) {     		
-				//aca se ejecuta cuando hay errores
-				console.log( response);     			
-		  });
- 		console.log("Modificar"+persona.id);
-		$http.post("PHP/nexo.php", {datos:{accion:"buscar", id:persona.id}})
-		.then(function(respuesta)
-		{
-			var persona=respuesta.data;
-			$state.go("persona.Alta");//location.href="formAlta.html";
-			$scope.DatoTest=persona.nombre;
-			console.log(persona);
-		} ,function errorCallback(response) {        
-			//aca se ejecuta cuando hay errores
-			console.log(response);           
-		});
-	}
+$scope.Modificar=function(persona)
+	{
+		$state.go("modificacion", persona);
+	};
 
 });
 miApp.controller("controlLogin",function($scope,$state){
@@ -343,3 +319,44 @@ $scope.Comenzar =function(){
 });
 
 
+miApp.controller('controlModificacion', function($scope, $http, $state, $stateParams, FileUploader)//, $routeParams, $location)
+{
+	$scope.persona={};
+	$scope.DatoTest="**Modificar**";
+	$scope.SubirdorArchivos = new FileUploader({url:'./servidor/archivos.php'});  $scope.persona={};
+	console.log($stateParams);//$scope.persona=$stateParams;
+	$scope.persona.id=$stateParams.id;
+	$scope.persona.nombre=$stateParams.nombre;
+	$scope.persona.apellido=$stateParams.apellido;
+	$scope.persona.dni=$stateParams.dni;
+	$scope.persona.foto=$stateParams.foto;
+
+	$scope.SubirdorArchivos.onSuccessItem=function(item, response, status, headers)
+	{
+		$http.post('PHP/nexo.php', { datos: {accion :"modificar",persona:$scope.persona}})
+		.then(function(respuesta) 
+		{
+			//aca se ejetuca si retorno sin errores      	
+			console.log(respuesta.data);
+			$state.go("persona.Grilla");
+		},
+		function errorCallback(response)
+		{
+			//aca se ejecuta cuando hay errores
+			console.log( response);     			
+		});
+		console.info("Ya guardé el archivo.", item, response, status, headers);
+	};
+
+	$scope.Modificar=function(persona)
+	{
+	
+		if($scope.SubirdorArchivos.queue[0]!=undefined)
+		{
+			var nombreFoto = $scope.SubirdorArchivos.queue[0]._file.name;
+			$scope.persona.foto=nombreFoto;
+		}
+		$scope.SubirdorArchivos.uploadAll();
+	}
+
+});
